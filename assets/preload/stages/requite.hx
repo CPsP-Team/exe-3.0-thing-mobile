e37f27c3548d8371795052a177b4012d6a0f645c
MaysLastPlay <clashtopclansonline@gmail.com>
2026-02-20 12:01:50 +0200

    forgor

--- assets/preload/stages/requite.hx
+++ assets/preload/stages/requite.hx
@@ -1,11 +1,32 @@
+import PlayState;
+
+var bg:FlxSprite;
+var floor:FlxSprite;
 function onCreate() {
-    var bg = new FlxSprite(-750, 50).loadGraphic(Paths.image("requital/marble_BGP1"));
+    bg = new FlxSprite(-750, 50).loadGraphic(Paths.image("requital/marble_BGP1"));
     bg.scale.set(1.5, 1.5);
     bg.antialiasing = true;
     add(bg);
 
-    var floor = new FlxSprite(-750, 50).loadGraphic(Paths.image("requital/marble_floorP1"));
+    floor = new FlxSprite(-750, 50).loadGraphic(Paths.image("requital/marble_floorP1"));
     floor.scale.set(1.5, 1.5);
     floor.antialiasing = true;
     add(floor);
+}
+
+function onUpdate(elapsed:Float) {
+    if (PlayState.SONG.song.toLowerCase()== 'forestall-desire') {
+    switch (curStep) {
+        case 784:
+            FlxG.camera.flash();
+
+            bg.alpha = 0;
+            floor.alpha = 0;
+        case 1311:
+            FlxG.camera.flash();
+
+            bg.alpha = 1;
+            floor.alpha = 1;
+    }
+  }
 }
